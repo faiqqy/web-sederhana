3ab7dbc4d3e8ed3c0d9f09a8ffc4173c696f4768
faiqqy <mufaiqabdillahtrianto@gmail.com>
2025-04-27 21:04:07 +0700

    first commit

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$URL=$_SERVER['HTTP_HOST'];
+$URI=$_SERVER['REQUEST_URI'];
+$query=$SERVER['QUERY_STRING'];
+
+echo $URL . $URI;
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "</head>";
+echo "<body>";
+
+echo "<h1>". "halo - halo bandung" . "</h1>";
+
+
+echo "</body>";
+echo "</html>";
+?>
+
